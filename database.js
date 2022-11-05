0cc54c764403fc4e7db8c991e10c5c935dd690ae
zakseger9 <zackary_seger@insideranken.org>
2022-11-04 19:14:45 -0500

    // 11/4 updateOneTestCaseToBug test-02

--- database.js
+++ database.js
@@ -346,11 +346,13 @@ async function insertOneEdit(edit) {
   });
 }
 
+// 11/4 updateOneTestCaseToBug test-02
+
 async function updateOneTestCaseToBug(bugId, testCaseId, update) {
   const db = await connect();
   return await db.collection('Bugs').updateOne(
     { $and: [ { _id: { $eq: bugId } }, { bugTestCases: { $elemMatch: { testId: { $eq: testCaseId } }}} ]},
-    { $set: {"bugTestCases.$.testId": testCaseId }}
+    { $set: {"bugTestCases.$.testId": "testCaseId" }}
   );
 }
 
