c1c006fc1fed7e1e07da0cffa13b4f14b77f86a3
zakseger9 <zackary_seger@insideranken.org>
2022-11-05 12:50:25 -0500

    -p

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -61,7 +61,7 @@ class LoginForm extends React.Component {
 
     loginCreds = Object.fromEntries(formData.entries());
 
-    console.log('credentials object: ' + JSON.stringify(loginCreds.email));
+    console.log('credentials object: ' + JSON.stringify(JSON.stringify(loginCreds)));
 
     const bds = new BugDataService(); 
 
