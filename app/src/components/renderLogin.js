5c37492c34df007d182c0c94a9be4187c08d160d
zakseger9 <zackary_seger@insideranken.org>
2022-11-05 07:01:23 -0500

    p

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -31,7 +31,7 @@ class LoginForm extends React.Component {
     this.emailInput = React.createRef();
     this.passwordInput = React.createRef();
 
-    this.focusInput = this.focusEmailInput.bind(this);
+    this.focusInput = this.focusInput.bind(this);
 
   }
 
