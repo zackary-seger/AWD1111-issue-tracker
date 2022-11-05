aa88099ab268f3ea11be50edbb4b07d2058a9397
zakseger9 <zackary_seger@insideranken.org>
2022-11-05 15:40:11 -0500

    p-100000000000

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -104,10 +104,10 @@ class LoginForm extends React.Component {
           <Form.Group className="mb-3" controlId="renderLogin.userCredentials">
 
             <Form.Label>Email address</Form.Label>
-            <Form.Control className="me-3" type="email" placeholder="name@example.com" controlId="emailInput" ref={this.emailInput} />
+            <Form.Control className="pe-3" type="email" placeholder="name@example.com" controlId="emailInput" ref={this.emailInput} />
 
             <Form.Label>Password</Form.Label>
-            <Form.Control className="me-3" type="password" controlId="passwordInput" ref={this.passwordInput} />
+            <Form.Control className="pe-3" type="password" controlId="passwordInput" ref={this.passwordInput} />
 
           </Form.Group>
 
