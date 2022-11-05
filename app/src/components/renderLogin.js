967556d895ab734e879385dd1ac787aed634c7ad
zakseger9 <zackary_seger@insideranken.org>
2022-11-05 03:33:26 -0500

    renderLogin.js Test-08

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -11,80 +11,76 @@ import BugDataService from "../services/bugList"
 import { useHistory } from "react-router-dom";
 
 let loginCreds;
+let savedToken;
 let x = 5;
 
-const LoginUser = (props) => { 
+const history = (useHistory);
 
-  const [authToken, setAuthToken] = useState([]); 
-  const history = (useHistory);
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
 
-  class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.input = React.createRef();
+  loginUser(event) { 
+
+    event.preventDefault();
+
+    const formData = new FormData();
+    loginCreds = Object.fromEntries(formData.entries());
+
+    BugDataService.login(loginCreds.emailInput, loginCreds.passwordInput).then( response => { 
+
+      console.log(response.data); 
+      savedToken = response.data;
+
+    }).catch( e =>{ 
+      console.log(e); 
+    }) 
+
+
+    if ( savedToken !== null && savedToken !== undefined ) {
+      history("/bugList", { replace: true });
+    } else {
+      console.log({ error: 'Invalid Credentials. Please Try Again.. (' + x + ' attempts remaining)' });
+      x--;
     }
-
-    loginUser(event) { 
-
-      event.preventDefault();
-
-      const formData = new FormData();
-      loginCreds = Object.fromEntries(formData.entries());
-
-      BugDataService.login(loginCreds.emailInput, loginCreds.passwordInput).then( response => { 
-
-        console.log(response.data); 
-        setAuthToken(response.data);
-
-      }).catch( e =>{ 
-        console.log(e); 
-      }) 
-
-      props.authToken = authToken;
-
-      if ( props.authToken !== null && props.authToken !== undefined ) {
-        history("/bugList", { replace: true });
-      } else {
-        console.log({ error: 'Invalid Credentials. Please Try Again.. (' + x + ' attempts remaining)' });
-        x--;
-      }
-      
-    } 
-
-    // Render HTML:
     
-    render() {
+  } 
 
-      return (
+  // Render HTML:
+  
+  render() {
 
-        <Form onSubmit={ LoginUser }>
+    return (
 
-          <Form.Group className="mb-3" controlId="renderLogin.userCredentials">
+      <Form onSubmit={ LoginUser }>
 
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" controlId="emailInput"/>
+        <Form.Group className="mb-3" controlId="renderLogin.userCredentials">
 
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" controlId="passwordInput"/>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" controlId="emailInput"/>
 
-          </Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" controlId="passwordInput"/>
 
-          <Form.Group className="mb-3" controlId="renderLogin.loginButton">
+        </Form.Group>
 
-            <Button 
-              variant="primary" 
-              type="submit" 
-            >
-              Login
-            </Button> 
-            
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="renderLogin.loginButton">
 
-        </Form>
+          <Button 
+            variant="primary" 
+            type="submit" 
+          >
+            Login
+          </Button> 
+          
+        </Form.Group>
 
-      );
-    }
+      </Form>
+
+    );
   }
 }
 
