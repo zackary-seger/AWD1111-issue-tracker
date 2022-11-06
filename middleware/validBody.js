0418b58df158c4b156897f06d558af8a68d72cd9
zakseger9 <zackary_seger@insideranken.org>
2022-11-05 19:23:10 -0500

    p

--- middleware/validBody.js
+++ middleware/validBody.js
@@ -1,6 +1,7 @@
 
 const validBody = ( schema ) => {
   return (req, res, next) => {
+    console.log('req.body: ' + req.body)
     const validateResult = schema.validate(req.body, { abortEarly: false });
     if (validateResult.error){
       return res.status(400).json({  error: `${validateResult.error}` });
