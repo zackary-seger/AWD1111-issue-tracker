3c342eba3e0ed733491e5e7d5a168db78ae13201
zakseger9 <zackary_seger@insideranken.org>
2022-11-09 12:53:36 -0600

    navBar Implemented as Component in App.js

--- app/src/App.js
+++ app/src/App.js
@@ -18,49 +18,6 @@ function App() {
     <div>
 
       <RenderNavbar />
-
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="ps-4 pe-4">
-      <Navbar.Brand href="#home" className="me-5">
-        <Logo
-          alt=""
-          width="30"
-          height="30"
-          className="d-inline-block align-top pe-2"
-        />
-        Bug Tracker v0.1
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-
-          <Nav className="mr-auto"></Nav>
-          <Nav>
-
-            <Nav.Link href="/">Login</Nav.Link>
-            <Nav.Link href="/user/list">List All Users</Nav.Link>
-            <Nav.Link href="/bug/list">List All Bugs</Nav.Link>
-            <Nav.Link href="/bug/:bugId">Find Bug By bugID</Nav.Link>
-
-            <NavDropdown title="Update Bug" id="collapsible-nav-dropdown">
-
-              <NavDropdown.Item href="#action/3.1">Update Existing Bug</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Classify Bug</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Assign Bug</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Close Bug</NavDropdown.Item>
-
-              <NavDropdown.Divider />
-
-              <NavDropdown.Item href="#action/3.5">Bug Test Case Options</NavDropdown.Item>
-
-            </NavDropdown>
-
-            <Nav.Link href="#deets">Create Bug</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">Register New User</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-
-        </Navbar>
   
       <Switch> 
 
