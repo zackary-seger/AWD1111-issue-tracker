0cdb6456e7764591b54e3ea43db51d90442faf3a
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 17:58:59 -0600

    p

--- app/src/components/loadBugList.js
+++ app/src/components/loadBugList.js
@@ -2,18 +2,20 @@ import { finalArr } from './bugList';
 
 const LoadBugList = props => { 
 
+    let obj1 = finalArr['0'];
+
     return (
-      <div>
-        <div className="card bug">
+
+        <div className="card">
 
           <img src="../images/img_avatar.png" alt="Avatar"></img>
           <div class="container">
-            <h4><b>{finalArr['0']}</b></h4>
+            <h4><b>{obj1}</b></h4>
             <p>Found By: Zackary Seger</p>
           </div>
 
         </div>
-      </div>  
+
     )
 
 
