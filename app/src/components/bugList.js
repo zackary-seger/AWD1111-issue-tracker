aa0d253b823a587b7121c3b3d145e7b7fdd2362f
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 14:13:26 -0600

    p

--- app/src/components/bugList.js
+++ app/src/components/bugList.js
@@ -1,7 +1,5 @@
-import React, {useState, useEffect, useCallback } from 'react' 
+import {useState, useCallback } from 'react' 
 import BugDataService from "../services/bugDataService" 
-import Cookies from 'js-cookie';
-import { Link } from "react-router-dom" 
 
 let bugArray;
 
@@ -13,7 +11,6 @@ const BugList = props => {
     const bds = new BugDataService('payload.email', 'payload.email');
     
     bds.bugList().then( response => { 
-    console.log(response.data) 
     return response;
   }).catch( e =>{ 
     console.log(e) 
