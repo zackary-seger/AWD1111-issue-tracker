6354bd6dac8169fee149024f937e5d1bbfecc0f7
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 01:10:14 -0600

    sweeeet bruh :D

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -54,10 +54,10 @@ function RenderNavbar() {
           </Nav>
         </Navbar.Collapse>
 
-        <Navbar.Brand id="navIcons" className="ms-3">
-          <FaShoppingCart className="me-1"/>
-          <FaUserCircle className="me-1"/> 
-          <FaAlignJustify className="me-1"/>
+        <Navbar.Brand id="navIcons" className="ms-3 pb-1">
+          <FaShoppingCart className="me-2"/>
+          <FaUserCircle className="me-2"/> 
+          <FaAlignJustify className="me-2"/>
         </Navbar.Brand>
 
         </Navbar>
