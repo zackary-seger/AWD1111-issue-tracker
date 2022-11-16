c08be8e5cdcafc71c353faf1f7f7736afc7ce753
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 14:23:29 -0600

    should pull one set of 5 bugs as json an log in browser console..

--- app/src/components/bugList.js
+++ app/src/components/bugList.js
@@ -2,22 +2,28 @@ import {useState, useCallback } from 'react'
 import BugDataService from "../services/bugDataService" 
 
 let bugArray;
+let x = 0;
 
 const BugList = props => { 
 
   const [bugs, setBugs] = useState([]) 
 
-  const response = useCallback(() =>{   
+  const response = useCallback(() =>{  
+
     const bds = new BugDataService('payload.email', 'payload.email');
     
     bds.bugList().then( response => { 
+    if ( x === 0 && response) {
+      setBugs(response);
+      x++;
+    }
+
     return response;
   }).catch( e =>{ 
     console.log(e) 
   }) 
   },[])
 
-  setBugs(response)
   bugArray = bugs;
   console.log(bugArray);
 
