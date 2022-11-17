25bffc5eddffcda83c69a8d80311a4091cb4831f
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 20:08:56 -0600

    p

--- app/src/components/loadBugList.js
+++ app/src/components/loadBugList.js
@@ -5,19 +5,26 @@ function LoadBugList() {
     let obj1 = finalArr['0'];
     console.log(obj1);
 
+    function RenderCard() { 
+      
+      return <div className="card">
+
+               <img src="../images/img_avatar.png" alt="Avatar"></img>
+               <div class="container">
+                 <h4><b>{obj1.title}</b></h4>
+                 <p>Found By: Zackary Seger</p>
+               </div>
+
+             </div>
+    
+    }
+
     if (obj1) {
 
       console.log(obj1.title);
 
-      return <div className="card">
-
-              <img src="../images/img_avatar.png" alt="Avatar"></img>
-              <div class="container">
-                <h4><b>{obj1.title}</b></h4>
-                <p>Found By: Zackary Seger</p>
-              </div>
-
-             </div>
+      const root = ReactDOM.createRoot(document.getElementById('root')); 
+      root.render(<RenderCard />)
 
       
     }
