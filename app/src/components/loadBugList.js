c3d59fdfada66c62d426a44b3097d6889dd82922
zakseger9 <zackary_seger@insideranken.org>
2022-11-16 18:10:25 -0600

    yeeYee!!

--- app/src/components/loadBugList.js
+++ app/src/components/loadBugList.js
@@ -1,24 +1,29 @@
 import { finalArr } from './bugList';
 
+let x = 0;
+
 const LoadBugList = props => { 
 
     let obj1 = finalArr['0'];
     console.log(obj1);
 
-    return (
+    if (x > 0) {
+      return (
 
-        <div className="card">
+          <div className="card">
 
-          <img src="../images/img_avatar.png" alt="Avatar"></img>
-          <div class="container">
-            <h4><b>{obj1.title}</b></h4>
-            <p>Found By: Zackary Seger</p>
+            <img src="../images/img_avatar.png" alt="Avatar"></img>
+            <div class="container">
+              <h4><b>{obj1.title}</b></h4>
+              <p>Found By: Zackary Seger</p>
+            </div>
+
           </div>
 
-        </div>
+      )
+    }
 
-    )
-
+    x++;
 
 }
 
