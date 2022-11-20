63216f31261b8916593e68db73f25b907d4b6138
zakseger9 <zackary_seger@insideranken.org>
2022-11-20 02:57:16 -0600

    pppp

--- middleware/hasAnyRole.js
+++ middleware/hasAnyRole.js
@@ -5,7 +5,7 @@ const hasAnyRole = () => {
       return res.status(401).json({ error: 'You are not logged in!' });
     } else if (req.auth.role && typeof req.auth.role === 'string') {
       return next();
-    } else if (Array.isArray(req.auth.role) && req.auth.role[0]) {
+    } else if (req.auth.role != null && req.auth.role[0]) {     // edit: changed to != null
       return next();
     } else {
       return res.status(403).json({ error: 'You have not been assigned a role!' });
