df11b3ffaff6eecbb0547b9b4d615c86349e0197
zakseger9 <zackary_seger@insideranken.org>
2022-11-20 03:27:34 -0600

    pppp

--- middleware/hasAnyRole.js
+++ middleware/hasAnyRole.js
@@ -4,9 +4,9 @@ const debugMain = Debug('app:routes:user');
 const hasAnyRole = () => {
   return (req, res, next) => {
     debugMain(req.auth);
-    if (!req.auth) {
+    if (!req.auth.role) {
       return res.status(401).json({ error: 'You are not logged in!' });
-    } else if (req.auth.role === 'null') {     // edit: added to handle nulls that are default..
+    } else if (req.auth.role === null) {     // edit: added to handle nulls that are default..
       return res.status(403).json({ error: 'You have not been assigned a role!' });
     } else if (req.auth.role && typeof req.auth.role === 'string') {
       return next();
--- routes/api/user.js
+++ routes/api/user.js
@@ -876,6 +876,7 @@ router.put('/login', validBody(loginSchema), async (req, res, next) => {
         const authMaxAge = parseInt(config.get('auth.cookieMaxAge'));
 
         res.cookie('authToken', authToken, { maxAge: authMaxAge, secure: true });
+        req.auth.role = user.role;
 
         debugMain(req.cookies);
         console.log('\n');
