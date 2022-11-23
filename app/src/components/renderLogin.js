7134a0adecf8d7323b938560530138c18dc1b675
zakseger9 <zackary_seger@insideranken.org>
2022-11-23 05:12:19 -0600

    777$$$777

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -98,7 +98,7 @@ class LoginForm extends React.Component {
 
           <a className='pb-2 ps-1' href='/#'>Forgot your password?</a>         
           <p className='d-block mb-0 mt-2 ps-1'>New to boatborrowers.com?</p>
-          <a className='ms-2 mb-2 ps-1' onClick={e => {e.preventDefault(); this._IsNewUser = true; }} href='/#'>
+          <a className='ms-2 mb-2 ps-1' onClick={e => {e.preventDefault(); this._IsNewUser = true; console.log(this._IsNewUser)}} href='/#'>
             Register Here!
           </a> 
 
