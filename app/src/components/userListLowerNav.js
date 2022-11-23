29aa818df6aa752ebef618fbebb4a40d49b5a603
zakseger9 <zackary_seger@insideranken.org>
2022-11-22 20:29:15 -0600

    ppp

--- app/src/components/userListLowerNav.js
+++ app/src/components/userListLowerNav.js
@@ -1,15 +1,18 @@
 import * as React from "react";
 import '../index.css';
 import MyXYControlledModal from "./userListFilterModal.js";
+import Fade from 'react-bootstrap/Fade';
 
 function RenderLowerNavbar() {
 
   const [modalShow, setModalShow] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   const ButtonClicked = (e) => {
                                   e.preventDefault();  
                                   console.log('Modal Attempts..');
-                                  setModalShow(true);                                     
+                                  setModalShow(true);
+                                  setOpen(!open);                                     
                                }
 
   return ( 
@@ -25,12 +28,21 @@ function RenderLowerNavbar() {
     </div>
 
     <div className='d-inline-block lowerNavLink'>
-      <a onClick={(evt) => ButtonClicked(evt)} id='bugListH2a' href='/bug/list'>
+      <a aria-expanded={open} aria-controls="xyModal" onClick={(evt) => ButtonClicked(evt)} id='bugListH2a' href='/bug/list'>
         <h2 id='bugListH202' className='text-primary mt-2 mb-4 me-2'>FILTER LIST ▼</h2>
       </a>
     </div>
 
-    <MyXYControlledModal show={modalShow} onHide={ e => { setModalShow(false); e.stopPropagation() } } />
+    <Fade in={open}>
+      <MyXYControlledModal
+        id="xyModal" 
+        show={modalShow} 
+        onHide={ e => { 
+                 setModalShow(false); 
+                 e.stopPropagation(); 
+               } } 
+      />
+    </Fade>
 
    </div>
 
