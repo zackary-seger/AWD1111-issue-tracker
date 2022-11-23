159a08168e71c2b1d1f10ddfe675ef836e5cc248
zakseger9 <zackary_seger@insideranken.org>
2022-11-23 02:17:16 -0600

    ppp

--- app/src/components/renderLogin.js
+++ app/src/components/renderLogin.js
@@ -78,12 +78,10 @@ class LoginForm extends React.Component {
 
     const validator = CreateValidator;
     
-    let rfInstance = new registerForm(this.props);
-    
     return (
       <div>
       
-      <rfInstance isNewUser={this.props.isNewUser} />
+      <registerForm isNewUser={this.props.isNewUser} />
 
       <body className=''>
         <h1 className="pt-3 ms-3">User Login  <FaSignInAlt id="signIn1" className="ms-2"/></h1>
