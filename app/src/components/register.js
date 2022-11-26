9b46ba689222faa28a579f0b43e63a1654bce99f
zakseger9 <zackary_seger@insideranken.org>
2022-11-26 17:30:55 -0600

    $$$$

--- app/src/components/register.js
+++ app/src/components/register.js
@@ -59,7 +59,7 @@ class RegisterForm extends React.Component {
     console.log(`isCancelledSave: ${isCancelledSave}`);
     console.log(z);
 
-    if ( (isNewUserSave && isCancelledSave === false && isRendered === false) || z > 0 ) { 
+    if (( isNewUserSave || this._isNewUser ) && isCancelledSave === false && isRendered === false) { 
       
       z++;
       console.log('Conditional Render Function return root.render() Commencing..');
