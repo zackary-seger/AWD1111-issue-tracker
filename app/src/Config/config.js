c3470ab159aec3a64aae5a65e3b23757a02ee64c
zakseger9 <zackary_seger@insideranken.org>
2022-12-27 00:12:18 -0600

    zs 15

--- app/src/Config/config.js
+++ app/src/Config/config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID || "<YOUR_CLIENT_ID>"
-export const APP_SECRET = process.env.APP_SECRET || "<YOUR_APP_SECRET>"
+export const CLIENT_ID = process.env.CLIENT_ID || "AYyCudAHZ1C7j5K5ib724pwwcIOb_pDzRD83Woc8p9zLV4Pv109POzEnZUcdBtuTzvXI"
+export const APP_SECRET = process.env.APP_SECRET || "EPz8rMCmFo2dB74AUHljCGrXPtGKdDMly2FH4j4nAvWJ1A4ZcVY8epGj7oIYYlPQINPgkqldj7cu-VWW"
