e1816c5f95d4affd7cb510cc01bd9b48b7a2121c
zakseger9 <zackary_seger@insideranken.org>
2022-12-27 00:34:14 -0600

    zs 18

--- app/src/Config/config.js
+++ app/src/Config/config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID || "AYyCudAHZ1C7j5K5ib724pwwcIOb_pDzRD83Woc8p9zLV4Pv109POzEnZUcdBtuTzvXI"
+export const CLIENT_ID = process.env.CLIENT_ID || "AYyCudAHZ1C7j5K5ib724pwwcIOb_pDzRD83Woc8p9zLV4Pv109POzEnZUcdBtuTzvXI-yvXKQ6nGiOG"
 export const APP_SECRET = process.env.APP_SECRET || "EPz8rMCmFo2dB74AUHljCGrXPtGKdDMly2FH4j4nAvWJ1A4ZcVY8epGj7oIYYlPQINPgkqldj7cu-VWW"
